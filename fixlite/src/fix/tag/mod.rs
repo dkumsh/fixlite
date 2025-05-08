b75abc5072fed9044139c610a70d069dd6fb6993
Dmitriy Kumshayev <dq@mail.com>
2025-05-08 13:28:42 +0100

    bumped version to 0.2.12: added metadata for tag 151

--- fixlite/src/fix/tag/mod.rs
+++ fixlite/src/fix/tag/mod.rs
@@ -83,6 +83,7 @@ impl DefaultRegistry {
         tag_metadata!(registry, "38", vec![type_name::<f64>()]);
         tag_metadata!(registry, "44", vec![type_name::<f64>()]);
         tag_metadata!(registry, "52", vec![type_name::<DateTime<Utc>>()]);
+        tag_metadata!(registry, "151", vec![type_name::<f64>()]);
         tag_metadata!(registry, "270", vec![type_name::<f64>()]);
         tag_metadata!(registry, "271", vec![type_name::<f64>()]);
         tag_metadata!(registry, "272", vec![type_name::<DateTime<Utc>>()]);
