57b9e2d55041567a1307ec7bdbcd889a62641e7f
Igor Khomyakov <ikh@ikhsoftware.com>
2025-09-08 17:39:30 -0600

    unsafe utf8

--- fixlite/src/lib.rs
+++ fixlite/src/lib.rs
@@ -19,13 +19,13 @@ pub enum FixError {
 
 pub trait FixDeserialize<'fix>: Sized {
     fn from_fix(fix_message: &'fix [u8]) -> Result<Self, FixError> {
-        let fix_message_str = std::str::from_utf8(fix_message)?;
+        let fix_message_str = unsafe { std::str::from_utf8_unchecked(fix_message) };
         let mut fields = fix_message_str.split(|c| c == '=' || c == '\x01');
         Self::deserialize_fields(&mut fields, |_| false)
     }
 
     fn from_fix_with_separator(fix_message: &'fix [u8], separator: char) -> Result<Self, FixError> {
-        let fix_message_str = std::str::from_utf8(fix_message)?;
+        let fix_message_str = unsafe { std::str::from_utf8_unchecked(fix_message) };
         let mut fields = fix_message_str.split(|c| c == '=' || c == separator);
         Self::deserialize_fields(&mut fields, |_| false)
     }
